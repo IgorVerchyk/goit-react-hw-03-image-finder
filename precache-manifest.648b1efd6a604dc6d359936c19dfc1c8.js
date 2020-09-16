f0842fb25e4fc29bf1fe66ef4e5ae291ff1916ec
IgorVerchyk <sabyr.ivua@gmail.com>
2020-09-16 18:51:13 +0300

    Updates

---
+++ precache-manifest.648b1efd6a604dc6d359936c19dfc1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db5c80f8b9aeb05c991b5cb69a20551",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "551eb9581dc48147bfc4",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.44660918.chunk.css"
+  },
+  {
+    "revision": "803695485dcfadd609c8",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3822d5b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3822d5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551eb9581dc48147bfc4",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.a7a3f671.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
